0b13d2e036f20588be82c331c76e0507a8003e5c
Ayyoub <ayyoub@musekits.com>
2026-03-16 04:21:34 -0400

    Added fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,17 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
 	title: "Ayyoub | Full Stack Developer",
-	description: "Ayyoub – Full-Stack Web Developer with 4+ years of experience building high-traffic websites, custom web apps, and scalable cloud architectures."
+	description: "Ayyoub – Full-Stack Web Developer with 4+ years of experience building high-traffic websites, custom web apps, and scalable cloud architectures.",
+	icons: {
+		icon: [
+			{ url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+			{ url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
+		],
+		apple: [
+			{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+		]
+	},
+	manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({
